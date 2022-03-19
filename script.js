a5e1c0b77ed2dbcc9c0f4c14f1fb181040ee63bc
joyomerebereubamaka <joyomerebereubamaka@gmail.com>
2022-03-19 15:13:02 +0100

    Beautiful places for tourist interest

---
+++ script.js
@@ -0,0 +1,97 @@
+onst change = document.getElementById('image1');
+
+change.addEventListener('click', (event) => {
+    event.target.style.background = 'blue';
+    event.target.style.color = 'pink';
+});
+
+const change2 = document.getElementById('image2');
+
+change2.addEventListener('click', (event) => {
+    event.target.style.background = 'red';
+    event.target.style.color = 'yellow';
+});
+
+const change3 = document.getElementById('image3');
+
+change3.addEventListener('click', (event) => {
+    event.target.style.background = 'green';
+    event.target.style.color = 'white';
+});
+
+const change4 = document.getElementById('image4');
+
+change4.addEventListener('click', (event) => {
+    event.target.style.background = 'gray';
+    event.target.style.color = 'crimson';
+});
+
+const change5 = document.getElementById('image5');
+
+change5.addEventListener('click', (event) => {
+    event.target.style.background = 'orchid';
+    event.target.style.color = 'purple';
+});
+
+const change6 = document.getElementById('image6');
+
+change6.addEventListener('click', (event) => {
+    event.target.style.background = 'teal';
+    event.target.style.color = 'maroon';
+});
+
+const change7 = document.getElementById('image7');
+
+change7.addEventListener('click', (event) => {
+    event.target.style.background = 'brown';
+    event.target.style.color = 'Navy';
+});
+
+const change8 = document.getElementById('image8');
+
+change8.addEventListener('click', (event) => {
+    event.target.style.background = 'purple';
+    event.target.style.color = 'orange';
+});
+
+const change9 = document.getElementById('image9');
+
+change9.addEventListener('click', (event) => {
+    event.target.style.background = 'lime';
+    event.target.style.color = 'olive';
+});
+
+const change10 = document.getElementById('image10');
+
+change10.addEventListener('click', (event) => {
+    event.target.style.background = 'maroon';
+    event.target.style.color = 'pink';
+});
+
+const change11 = document.getElementById('image11');
+
+change11.addEventListener('click', (event) => {
+    event.target.style.background = 'violet';
+    event.target.style.color = 'blue';
+});
+
+const change12 = document.getElementById('image12');
+
+change12.addEventListener('click', (event) => {
+    event.target.style.background = 'teal';
+    event.target.style.color = 'gray';
+});
+
+const change13 = document.getElementById('image13');
+
+change13.addEventListener('click', (event) => {
+    event.target.style.background = 'brown';
+    event.target.style.color = 'black';
+});
+
+const change14 = document.getElementById('image14');
+
+change14.addEventListener('click', (event) => {
+    event.target.style.background = 'gold';
+    event.target.style.color = 'orange';
+});
